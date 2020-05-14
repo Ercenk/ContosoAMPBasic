bacd5e576d642ab12782f56d0f7adb323219b36c
Ercenk Keresteci <ercenk@ercenk.com>
2020-05-13 17:00:26 -0700

    Added term start end dates to the dashboard view. Moved solution file

--- src/Dashboard/Models/SubscriptionViewModel.cs
+++ src/Dashboard/Models/SubscriptionViewModel.cs
@@ -47,16 +47,25 @@ namespace Dashboard.Models
         public static SubscriptionViewModel FromSubscription(Subscription marketplaceSubscription)
         {
             return new SubscriptionViewModel
-                       {
-                           PlanId = marketplaceSubscription.PlanId,
-                           Quantity = marketplaceSubscription.Quantity,
-                           SubscriptionId = marketplaceSubscription.SubscriptionId,
-                           OfferId = marketplaceSubscription.OfferId,
-                           State = marketplaceSubscription.SaasSubscriptionStatus,
-                           SubscriptionName = marketplaceSubscription.Name,
-                           PurchaserEmail = marketplaceSubscription.Purchaser.EmailId,
-                           PurchaserTenantId = marketplaceSubscription.Purchaser.TenantId
-                       };
+            {
+                PlanId = marketplaceSubscription.PlanId,
+                Quantity = marketplaceSubscription.Quantity,
+                SubscriptionId = marketplaceSubscription.SubscriptionId,
+                OfferId = marketplaceSubscription.OfferId,
+                State = marketplaceSubscription.SaasSubscriptionStatus,
+                SubscriptionName = marketplaceSubscription.Name,
+                PurchaserEmail = marketplaceSubscription.Purchaser.EmailId,
+                PurchaserTenantId = marketplaceSubscription.Purchaser.TenantId,
+                TermStart = marketplaceSubscription.Term != null 
+                    && marketplaceSubscription.Term.StartDate != DateTimeOffset.MinValue ? 
+                        marketplaceSubscription.Term.StartDate.ToString("d") : 
+                        string.Empty,
+                TermEnd = marketplaceSubscription.Term != null && 
+                    marketplaceSubscription.Term.EndDate != DateTimeOffset.MinValue ? 
+                        marketplaceSubscription.Term.EndDate.ToString("d") : 
+                        string.Empty,
+                TermUnit = marketplaceSubscription.Term.TermUnit == TermUnitEnum.P1M ? "Monthly" : "Annual"
+            };
         }
 
         public Guid PurchaserTenantId { get; set; }
@@ -64,5 +73,9 @@ namespace Dashboard.Models
         public string PurchaserEmail { get; set; }
         public bool ExistingOperations { get; internal set; }
         public int OperationCount { get; internal set; }
+
+        public string TermStart { get; private set; }
+        public string TermEnd { get; private set; }
+        public string TermUnit { get; private set; }
     }
 }
